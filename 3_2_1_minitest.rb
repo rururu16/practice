d1e16f15a2652138eb1b7a11bfded5258e99338d
rururu16 <ynk618172@gmail.com>
2019-05-30 22:36:15 +0900

    Add fizz buzz test

--- 3_2_1_minitest.rb
+++
@@ -1,8 +0,0 @@
-require 'minitest/autorun'
-
-class SampleTest < Minitest::Test
-  def test_sample
-    assert_equal 'RUBY', 'ruby'.upcase
-    assert_equal 'Ruby on rails', 'ruby on rails'.capitalize
-  end
-end
--- ruby_book/test/2_fizz_buzz_test.rb
+++ ruby_book/test/2_fizz_buzz_test.rb
@@ -1,17 +1,5 @@
-def fizz_buzz(n)
-  if n % 15 == 0
-    'Fizz Buzz'
-  elsif n % 3 == 0
-    'Fizz'
-  elsif n % 5 == 0
-    'Buzz'
-  else
-    n.to_s
-  end
-end
-# puts fizz_buzz 15
-
 require 'minitest/autorun'
+require './lib/2_fizz_buzz'
 
 class FizzBuzzTest < MiniTest::Test
   def test_fizz_buzz
